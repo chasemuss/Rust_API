4bf92a74f8b206b2270abcb9702820b92f859c7c
chasemuss <chasemuss@gmail.com>
2026-01-18 21:54:15 -0600

    added prettifying stuff

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
 use actix_web::{ get, web, App, HttpResponse, HttpServer, Responder };
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct CalcResponse {
+    a: f64,
+    symbol: String,
+    b: f64,
+    result: f64,
+}
+
 
 #[get("/{operation}/{a}/{b}")]
 async fn calculate(path: web::Path<(String, f64, f64)>) -> impl Responder {
+    fn op_symbol(operation: & str) -> &'static str {
+        match operation.to_lowercase().as_str() {
+            "add"      => "+",
+            "subtract" => "-",
+            "multiply" => "*",
+            "divide"   => "/",
+            _          => "?",
+        }
+    }
+    
     let (operation, a, b) = path.into_inner();
-    println!("Calculating {} {} {}", a, operation, b);
+    println!("Calculating {} {} {}", a, op_symbol(&operation), b);
 
     let result = match operation.to_lowercase().as_str() {
-        "add" => a + b,
+        "add"      => a + b,
         "subtract" => a - b,
         "multiply" => a * b,
-        "divide" => {
+        "divide"   => {
             if b == 0.0 {
                 return HttpResponse::BadRequest().body("Division by zero is not permitted");
             }
@@ -20,7 +40,17 @@ async fn calculate(path: web::Path<(String, f64, f64)>) -> impl Responder {
         }
     };
 
-    HttpResponse::Ok().body(format!("{} {} {} = {}", a, operation, b, result))
+    let symbol = op_symbol(&operation).to_string();
+
+    let response = CalcResponse{
+        a: a, 
+        symbol: symbol,
+        b: b, 
+        result: result
+    };
+
+    println!("{} {} {} = {}", a, symbol, b, result);
+    HttpResponse::Ok().json(response)
 }
 
 
